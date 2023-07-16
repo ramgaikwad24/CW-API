18acf1dc7b824336f4719ddac6dbdb7c69f106f5
Rameshwar Gaikwad <gaikwadrameshwar99@gmail.com>
2023-07-16 21:04:53 +0530

    first commit

---
+++ src/main/java/com/ashokit/IhisAdminApiApplication.java
@@ -0,0 +1,18 @@
+package com.ashokit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.ashokit.entity.AppPlanEntity;
+
+@SpringBootApplication
+public class IhisAdminApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IhisAdminApiApplication.class, args);
+		
+		
+		
+	}
+
+}
